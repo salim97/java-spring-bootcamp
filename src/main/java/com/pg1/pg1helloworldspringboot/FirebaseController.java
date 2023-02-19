3c238ba48b5e84d6ed8e75ed373219fcf698cfc7
Salim Benabadji <benabadji.mohammed.salim@gmail.com>
2023-02-19 23:42:48 +0100

    wrap up

--- src/main/java/com/pg1/pg1helloworldspringboot/FirebaseController.java
+++ src/main/java/com/pg1/pg1helloworldspringboot/FirebaseController.java
@@ -1,10 +1,13 @@
 package com.pg1.pg1helloworldspringboot;
 
 import com.google.firebase.auth.FirebaseAuth;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.google.firebase.auth.ActionCodeSettings;
+import com.google.firebase.auth.FirebaseAuthException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class FirebaseController {
@@ -15,8 +18,44 @@ public class FirebaseController {
         return "hello to Spring boot world!";
     }
 
+
     @PostMapping("/resetPassword")
-    public String resetPassword(@RequestBody ResetPasswordDTO resetPasswordDTO){
-        return resetPasswordDTO.getEmail();
+    public Map<String, Object> resetPassword(@RequestBody ResetPasswordDTO resetPasswordDTO) throws FirebaseAuthException {
+
+        // 1) prepare template for request language
+        // 2) generate link
+        // 3) send link using SMTP ( request email )
+
+        /* // translate template according to request language
+        I18n i18n = I18n.builder()
+                .locale(resetPasswordDTO.getLanguageCode())
+                .addDefaultResourceBundle("i18n/password-reset")
+                .build();
+        */
+
+        ActionCodeSettings actionCodeSettings = ActionCodeSettings.builder()
+                .setUrl("https://spinarena.net/reset-password" )
+                .setHandleCodeInApp(true)
+                .build();
+
+        String link = FirebaseAuth.getInstance().generatePasswordResetLink(resetPasswordDTO.getEmail(), actionCodeSettings);
+
+//        sendEmail(email, message, link); // Implement this method to send the email.
+
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("success", true);
+        response.put("reset link", link);
+        return response;
+
+    }
+
+    @ExceptionHandler(FirebaseAuthException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, Object> handleFirebaseAuthException(FirebaseAuthException e) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("errorCode", e.getErrorCode());
+        response.put("errorMessage", e.getMessage());
+        return response;
     }
 }
--- src/main/java/com/pg1/pg1helloworldspringboot/ResetPasswordDTO.java
+++ src/main/java/com/pg1/pg1helloworldspringboot/ResetPasswordDTO.java
@@ -2,10 +2,12 @@ package com.pg1.pg1helloworldspringboot;
 
 import lombok.Getter;
 import lombok.Setter;
-
+import javax.validation.constraints.Email;
 @Setter
 @Getter
 public class ResetPasswordDTO {
+
+    @Email
     private String email;
     private String languageCode;
 }
